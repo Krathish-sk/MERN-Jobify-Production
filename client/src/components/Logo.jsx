e2fce457ca2fa9ea1de9ee8ad6542e5496e2d0c1
Krathish K <krathishs.k123@gmail.com>
2025-09-21 18:54:50 +0530

    Typo

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -5,7 +5,7 @@ import LogoImage from "../assets/images/logo.svg";
 export default function Logo() {
   return (
     <Link to={"/"}>
-      <img src={LogoImage} alt="Jobify Logo" className="logo" />;
+      <img src={LogoImage} alt="Jobify Logo" className="logo" />
     </Link>
   );
 }
